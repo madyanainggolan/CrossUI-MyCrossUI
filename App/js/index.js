eb8f2fb23f5b2bbcc4faa79ba2138898d21ea225
madyanainggolan <37835702+madyanainggolan@users.noreply.github.com>
2019-10-28 21:53:03 +0700

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,62 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label8")
+                .setLeft("16.666666666666668em")
+                .setTop("2.5em")
+                .setWidth("8.25em")
+                .setHeight("2em")
+                .setCaption("Biodata Diri Siswa")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input6")
+                .setDirtyMark(false)
+                .setLeft("2.5em")
+                .setTop("5.833333333333333em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Nama Siswa")
+                .setValue("Madya")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input7")
+                .setDirtyMark(false)
+                .setLeft("2.5em")
+                .setTop("9.166666666666666em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("No Induk Siswa")
+                .setValue("123456")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input8")
+                .setDirtyMark(false)
+                .setLeft("2.5em")
+                .setTop("12.5em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Alamat")
+                .setValue("Jalan Serpong")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button5")
+                .setDirtyMark(false)
+                .setLeft("16.666666666666668em")
+                .setTop("15.833333333333334em")
+                .setCaption("Input")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
